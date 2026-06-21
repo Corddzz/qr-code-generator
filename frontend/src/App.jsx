d1ca9c18b5d57bd84c1300fb1bd952fb051c6b84
Leonheil Cordero <leozcordz@gmail.com>
2026-06-21 20:52:39 +0800

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-zinc-800 size-56 rounded-lg p-4">
+        <h1 className="text-white text-2xl font-bold text-center mb-4">
+          QR Code Generator
+        </h1>
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
