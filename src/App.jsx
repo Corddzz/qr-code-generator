4969014ef3225f3837caa49ae6a9d995651bd09a
Leonheil Cordero <leozcordz@gmail.com>
2026-06-27 19:02:05 +0800

    refactor: remove unused download functionality and clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,6 @@ const App = () => {
     setShowQRCode(true);
   };
 
-  const handleDownload = () => {
-    const downloadLink = document.createElement("a");
-    downloadLink.href = qrCodeURL;
-    downloadLink.download = `${inputValue}_qr_code.png`;
-    downloadLink.click();
-  };
-
   return (
     <div className="flex items-center justify-center h-screen bg-zinc-800 flex-col gap-4">
       <div className="w-80 bg-zinc-600/30 rounded-xl p-5 shadow-lg border border-zinc-400/30">
@@ -72,49 +65,6 @@ const App = () => {
             >
               Generate QR Code
             </button>
-
-            {/* <button
-              // onClick={handleCopy}
-              className="bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-md"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-copy-icon lucide-copy"
-              >
-                <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
-                <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
-              </svg>
-            </button>
-
-            <button
-              onClick={handleDownload}
-              className="bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-md"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-download-icon lucide-download"
-              >
-                <path d="M12 15V3" />
-                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                <path d="m7 10 5 5 5-5" />
-              </svg>
-            </button> */}
           </div>
         )}
       </div>
